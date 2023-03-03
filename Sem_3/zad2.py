9522fa53f23bc4c29c46b9c5a165b691215cba97
PavelE13 <94640966+PavelE13@users.noreply.github.com>
2023-03-03 20:50:36 +0300

    Update Sem_3/zad2.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Sem_3/zad2.py
+++ Sem_3/zad2.py
@@ -12,13 +12,13 @@
 import random
 import math
 
-a=int(input('Введите количество элементов в массиве N: '))
-my_lst=[random.randint(0,a) for _ in range(a)]
-print (*my_lst)
-new_lst=[]
-x=int(input('введите некоторое число x='))
-for i in range(0,a):
-    new_lst.append(math.fabs(x-my_lst[i]))
-min_el=min(new_lst)
-min_ind=new_lst.index(min_el)
+a = int(input('Введите количество элементов в массиве N: '))
+my_lst = [random.randint(0, a) for _ in range(a)]
+print(*my_lst)
+new_lst = []
+x = int(input('введите некоторое число x='))
+for i in range(0, a):
+    new_lst.append(math.fabs(x - my_lst[i]))
+min_el = min(new_lst)
+min_ind = new_lst.index(min_el)
 print('Ближайшее числу х значение в списке= {}'.format(my_lst[min_ind]))
